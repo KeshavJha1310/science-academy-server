b4d77a650525e899b8ad8848a5fd16894d849102
keshavjha1310 <keshavjhainft-@atharvacoe.ac.in>
2025-03-10 02:07:22 +0530

    admin login updates

--- Controllers/admin_creds_controllers.js
+++ Controllers/admin_creds_controllers.js
@@ -49,6 +49,7 @@ module.exports.loginAdmins = async (req, res) => {
         let user = await adminModel.findOne({ AdminId: AdminId  });
 
         if (user) {
+            console.log(user)
             const isPasswordValid = user.AdminPassword === AdminPassword;
             if (isPasswordValid) {
                 // Convert the user object to a plain JavaScript object
